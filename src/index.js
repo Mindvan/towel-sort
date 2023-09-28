e2aa24bc608c2e3dbcf711d2252a98b1a9a14d16
Ivan Eroshin <itseroshin@gmail.com>
2023-09-28 23:03:27 +0300

    fead: add solution

--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    var res = [];
+
+    for (const part in matrix) {
+        part % 2 !== 0 ? res.push((matrix[part].reverse())) : res.push((matrix[part]));
+    }
+
+    return res.flat(Infinity);
 }
